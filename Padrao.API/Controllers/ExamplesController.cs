0fe3efb21bc1be0e5c26d43b5f70b539c325630f
obrusilva <BRUNOPDP@GMAIL.COM>
2024-02-14 21:27:20 -0300

    :sparkles: novo: examplo rate limiting

---
+++ Padrao.API/Controllers/ExamplesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+using Padrao.APi.Controllers;
+using Padrao.Domain.Interfaces;
+using System.Threading;
+
+namespace Padrao.Api.Controllers
+{
+    public class ExamplesController(IResponse response) : BaseController(response)
+    {
+        [HttpGet]
+        [EnableRateLimiting("ip")]
+        [Route("rate-limiting")]
+        public IActionResult RateLimitingIP()
+        {
+            Thread.Sleep(10000);
+            return JsonResponse();
+        }
+        
+        [HttpPost]
+        [EnableRateLimiting("concurrency")]
+        [Route("rate-limiting-post")]
+        public IActionResult ReteLimitingPost()
+        {
+            Thread.Sleep(10000);
+            return JsonResponse();
+        }
+    }
+}
--- Padrao.API/Program.cs
+++ Padrao.API/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -13,10 +14,13 @@ using Padrao.Domain.Interfaces;
 using Padrao.Domain.Virtual;
 using Padrao.Service.Interface;
 using Padrao.Service.Services;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
 using System.Text.Json;
+using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -61,6 +65,64 @@ builder.Services.Configure<RequestLocalizationOptions>(options =>
 
 
 builder.Services.AddHttpContextAccessor();
+builder.Services.AddRateLimiter(opt =>
+{
+    opt.RejectionStatusCode = StatusCodes.Status429TooManyRequests; //força o retorno 429 de too many requests
+    opt.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+        RateLimitPartition.GetFixedWindowLimiter(
+            partitionKey: $"{httpContext.User.Identity?.Name}_{httpContext.Request.Path}",
+            factory: partion =>
+            {
+                return new FixedWindowRateLimiterOptions { 
+                    AutoReplenishment = true,
+                    PermitLimit = 120,
+                    QueueLimit = 0,
+                    Window =TimeSpan.FromSeconds(60)
+                    };
+            })
+    );
+
+    opt.AddPolicy("ip", httpContext =>
+       RateLimitPartition.GetFixedWindowLimiter(
+           partitionKey: $"{httpContext.Request.Path}_{httpContext.Connection.RemoteIpAddress}",
+           factory: partition =>
+           {
+               return new FixedWindowRateLimiterOptions
+               {
+                   AutoReplenishment = true,
+                   PermitLimit = 1,
+                   QueueLimit = 0,
+                   Window = TimeSpan.FromSeconds(60)
+               };
+           }));
+    opt.AddPolicy("concurrency", httpContext =>
+        RateLimitPartition.GetConcurrencyLimiter(
+            partitionKey: httpContext.Request.Path,
+            factory: partition =>
+            {
+                return new ConcurrencyLimiterOptions { 
+                    PermitLimit = 1,
+                    QueueLimit = 0,
+                };
+            }));
+
+
+    opt.OnRejected = async (context, token) =>
+    {
+        context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+        if (context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAtfer))
+            await context.HttpContext.Response.WriteAsJsonAsync(new ResultJson($"O limite de requisições foi atingifo tente novamente daqui {retryAtfer.TotalSeconds} segundos ", null));
+        else if (context.Lease.TryGetMetadata(MetadataName.ReasonPhrase, out var reasonPhrase))
+            await (context.HttpContext.Response.WriteAsJsonAsync(new ResultJson($"O limite de requisições simultâneas foi atingido,tente novamente mais tarde", null), cancellationToken: token));
+        else
+            await (context.HttpContext.Response.WriteAsJsonAsync(new ResultJson($"o limite de requisições foi atingido, tente novamente mais tarde", null), cancellationToken: token));
+
+
+
+    };
+});
+
+
 builder.Services.AddScoped<IResponse, Response>();
 builder.Services.AddScoped<DataContext, DataContext>();
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
@@ -81,6 +143,9 @@ app.UseCors(x => x
     .AllowAnyMethod()
     .AllowAnyHeader()
 );
+
+app.UseRateLimiter();
+
 app.UseEndpoints(endpoints => { _ = endpoints.MapControllers(); });
 
 app.UseResponseCompression();
